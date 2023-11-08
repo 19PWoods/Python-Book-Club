84027f122635a1062d64f9d56b060cf239bd2285
Philip Woods, M.S <19PWoods@gmail.com>
2023-11-07 22:08:39 -0600

    For Loops

---
+++ chapter4.py
@@ -0,0 +1,35 @@
+# For Loops
+# magicians = ['alice', 'david', 'caroline']
+# for magician in magicians:
+#     print(magician)
+
+# magicians = ['alice', 'david', 'caroline']
+# for magician in magicians:
+#     print(f"{magician.title()}, that was a great trick!")
+
+# magicians = ['alice', 'david', 'caroline']
+# for magician in magicians:
+#     print(f"{magician.title()}, that was a great trick!")
+#     print(f"I can't wait to see your next trick, {magician.title()}.\n")
+
+# print("Thank you, everyone. That was a great magic show!")
+
+# for value in range(1, 5):
+#     print(value)
+
+# for value in range(25):
+#     print(value)
+
+# numbers = list(range(6))
+# print(numbers)
+
+# even_numbers = list(range(2, 11, 2))
+# print(even_numbers)
+
+squares = []
+for value in range(1, 11):
+    squares.append(value ** 2)
+    # square = value ** 2
+    # squares.append(square)
+
+print(squares)
