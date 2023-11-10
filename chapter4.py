53d2b5214677208741255b8e767a2220e2ba5adb
Philip Woods, M.S <19PWoods@gmail.com>
2023-11-10 13:06:54 -0600

    Chapter 4: for loop notes

--- chapter4.py
+++ chapter4.py
@@ -26,10 +26,60 @@
 # even_numbers = list(range(2, 11, 2))
 # print(even_numbers)
 
-squares = []
-for value in range(1, 11):
-    squares.append(value ** 2)
-    # square = value ** 2
-    # squares.append(square)
+# squares = []
+# for value in range(1, 11):
+#     squares.append(value ** 2)
+#     # square = value ** 2
+#     # squares.append(square)
 
-print(squares)
+# print(squares)
+
+# # Simple Statistics with List Numbers
+# digits = list(range(0, 10))
+# print(min(digits))
+# print(max(digits))
+# print(sum(digits))
+
+# # List comprehension: For loops in one line of code
+# squares = [value**2 for value in range(1, 11)]
+# print(squares)
+
+# # Slicing a list: Accessing certain index of a list
+# players = ['charles', 'martina', 'michael', 'florence', 'eli']
+# print(players[0:3])
+# print(players[1:4])
+# print(players[:4])  # automatically starts at beginning
+# print(players[2:])
+# print(players[-3:])
+
+# # Looping through a slice
+# print("Here are the first players on my team:")
+# for player in players[:3]:  # looping only through first three players
+#     print(player.title())
+
+# # Copying a list
+# my_foods = ['pizza', 'falafel', 'carrot cake']
+# friend_foods = my_foods[:]
+
+# my_foods.append('cannoli')
+# friend_foods.append('ice cream')
+
+# print(my_foods)
+# print(friend_foods)
+
+# # friend_foods = my_foods (this does not work, Python reads this as same list)
+
+# Defining a tuple - use () instead of []
+dimensions = (200, 50)
+print(dimensions[0])
+print(dimensions[1])
+# dimensions[0] = 250 this does not work, you can't change a tuple!!
+# in order to modify a tuple you must change the entire tuple
+print("Original dimensions")
+for dimension in dimensions:
+    print(dimensions)
+
+dimensions = (400, 100)
+print("\nModified dimensions:")
+for dimension in dimensions:
+    print(dimensions)
