2dbe783113cf09c9f8651b034d8d70d1da4b1c93
Philip Woods, M.S <19PWoods@gmail.com>
2023-11-17 15:26:27 -0600

    added monty carlo example from bob in class

--- chapter5.py
+++ chapter5.py
@@ -1,17 +1,17 @@
-# # If Statements
+# # # If Statements
 # cars = ['audi', 'bmw', 'subaru', 'toyota']
 
-# for car in cars:
-#     if car == 'bmw':
-#         print(car.upper())
-#     else:
-#         print(car.title())
+# # for car in cars:
+# if car == 'bmw':
+#     print(car.upper())
+# else:
+#     print(car.title())
 
-# Case sensitivity in Python
+# # Case sensitivity in Python
 # car = 'Audi'
 # print(car.lower() == 'audi')
 
-# Checking for inequality
+# # Checking for inequality
 # requested_topping = 'mushrooms'
 
 # if requested_topping != 'anchovies':
@@ -32,23 +32,23 @@
 # print('mushrooms' in requested_toppings)
 # print('pepperoni' in requested_toppings)
 
-# # Checking value IS NOT in list
+# # # Checking value IS NOT in list
 # banned_users = ['andrew', 'carolina', 'david']
 # user = 'marie'
 # if user not in banned_users:
 #     print(f"{user.title()}, you can post a response if you wish.")
 
-# Boolean Expressions
+# # Boolean Expressions
 # game_active = True
 # can_edit = False
 
-# # If statements
+# # # If statements
 # age = 19
 # if age > 18:
 #     print("You can vote!")
 #     print("Have you registered to vote?")
 
-# age = 17
+# # age = 17
 # if age > 18:
 #     print("\nYou can vote!")
 #     print("Have you registered to vote?")
@@ -56,7 +56,7 @@
 #     print("\nSorry, no voting for you!")
 #     print("Please register as soon as you are 18!")
 
-age = 72
+# age = 72
 # if age < 4:
 #     print("You are free admission!")
 # elif age < 18:
@@ -104,36 +104,73 @@ age = 72
 #     print("Adding extra cheese.")
 # print("\nFinished making your pizza!")
 
-# Using if statements with lists
-requested_toppings = ['mushrooms', 'green peppers', 'extra cheese']
+# # Using if statements with lists
+# requested_toppings = ['mushrooms', 'green peppers', 'extra cheese']
+
+# # for requested_toppings in requested_toppings:
+# #     print(f"Adding {requested_toppings}.")
+# # print("\nFinished making your pizza!")
+
+# # for requested_toppings in requested_toppings:
+# #     if requested_toppings == 'green peppers':
+# #         print("Sorry, out of green peppers.")
+# #     else:
+# #         print(f"Adding {requested_toppings}.")
+# # print("\nFinished making your pizza!")
+
+# # requested_toppings = []
+# # if requested_toppings:
+# #     for requested_toppings in requested_toppings:
+# #         print(f"Adding {requested_toppings}.")
+# #     print("\nFinished making your pizza!")
+# # else:
+# #     print(f"Are you sure you want a plain pizza?.")
+
+# # Using Multiple Lists
+# available_toppings = ['mushrooms', 'olives',
+#                       'green peppers', 'pepperoni', 'pineapple', 'extra cheese']
+# requested_toppings = ['mushrooms', 'french fries', 'extra cheese']
 
 # for requested_toppings in requested_toppings:
-#     print(f"Adding {requested_toppings}.")
-# print("\nFinished making your pizza!")
-
-# for requested_toppings in requested_toppings:
-#     if requested_toppings == 'green peppers':
-#         print("Sorry, out of green peppers.")
+#     if requested_toppings in available_toppings:
+#         print(f"Adding {requested_toppings}.")
 #     else:
-#         print(f"Adding {requested_toppings}.")
+#         print(f"Sorry, we don't have {requested_toppings}.")
 # print("\nFinished making your pizza!")
 
-# requested_toppings = []
-# if requested_toppings:
-#     for requested_toppings in requested_toppings:
-#         print(f"Adding {requested_toppings}.")
-#     print("\nFinished making your pizza!")
-# else:
-#     print(f"Are you sure you want a plain pizza?.")
 
-# Using Multiple Lists
-available_toppings = ['mushrooms', 'olives',
-                      'green peppers', 'pepperoni', 'pineapple', 'extra cheese']
-requested_toppings = ['mushrooms', 'french fries', 'extra cheese']
+# random.shuffle() randomly shuffles array values
+# Example from Bob - Monty Carlo
 
-for requested_toppings in requested_toppings:
-    if requested_toppings in available_toppings:
-        print(f"Adding {requested_toppings}.")
-    else:
-        print(f"Sorry, we don't have {requested_toppings}.")
-print("\nFinished making your pizza!")
+import random
+
+number_of_shots = 5
+possible_alien_colors = ['red', 'orange',
+                         'yellow', 'green', 'blue', 'black', 'violet']
+points = 0
+total_points = 0
+
+user_input = input(
+    "\n\nHow many simulations would you like to run? Simulations: ")
+simulations = int(user_input)
+
+for run_number in range(1, simulations+1):
+    # print(run_number)
+    for shot in range(1, number_of_shots+1):
+        random.shuffle(possible_alien_colors)
+        shot_down_color = possible_alien_colors[0]
+        if shot_down_color == 'green':
+            points = points + 5
+        if shot_down_color == 'yellow':
+            points = points + 10
+        if shot_down_color == 'red':
+            points = points + 15
+        if shot_down_color == 'orange' or shot_down_color == 'blue' or shot_down_color == 'black' or shot_down_color == 'violet':
+            points = points + 1
+        # print(f"Shot {shot}: You shot a {shot_down_color} alien. You now have {points} points!")
+    total_points = total_points + points
+    points = 0
+print(
+    f"\nDone. After {simulations} runs you have a grand total of {total_points} points.")
+average = total_points/simulations
+print(f"That's an average of {average} points per game!")
