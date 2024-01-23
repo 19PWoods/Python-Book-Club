491ea315ade74eb0e376eee909f96292421567b7
Philip Woods, M.S <19PWoods@gmail.com>
2024-01-23 11:30:25 -0600

    added mari's code

--- modulo_challeng.py
+++ modulo_challeng.py
@@ -26,7 +26,7 @@ start_time = timeit.default_timer()
 prime_numbers = [2, 3, 5, 7]
 num = 10
 division = list(range(2, 11))
-while len(prime_numbers) < 100000:
+while len(prime_numbers) < 100:
     remainders = []
     for value in division:
         remainders.append(num % value)
@@ -38,3 +38,31 @@ print(prime_numbers)
 stop_time = timeit.default_timer()
 print('Runtime (seconds): ', stop_time - start_time)
 print('\nLength of prime_numbers"', len(prime_numbers))
+
+# #A Program to list the first X Prime numbers
+# import math
+# import timeit
+
+# start_time = timeit.default_timer()
+
+# x=0
+# number=3
+# primes = [2]
+# extent = 100000 #modify to switch the number of primes reported
+# while len(primes)<extent:
+#     x=0
+#     if number%2 ==0:
+#         number=number+1
+#     else:
+#         for num in primes:
+#             if num > math.sqrt(number):
+#                 break
+#             elif number%num==0:
+#                 number=number+1
+#                 x=1
+#                 break
+#         if x!= 1:
+#             primes.append(number)
+#             number=number+1
+# #print(f"\nThe first {extent} prime numbers are:\n")
+# print(primes)
